7446c431ef07a51ee47577710e16989eec667a09
Roman Chaban <roman.chaban1001@gmail.com>
2024-03-22 15:41:20 +0200

    Fixed any components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { FC } from 'react';
 import './App.css';
-import { ArtBucket } from './components/ArtBucket/ArtBucket';
 
 export const App: FC = () => {
-  return (
-    <>
-      <ArtBucket />
-    </>
-  );
+  return <></>;
 };
